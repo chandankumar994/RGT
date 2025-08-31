c88e555af3baa1c6b3d0b132fcd55e872818348b
chandankumar994 <chandankumar994@gmail.com>
2025-08-31 20:41:52 +0530

    adding php section

---
+++ PHP/test.php
@@ -0,0 +1,17 @@
+<?php
+    echo "Hello Ravi";
+?>
+
+<h1>This is h1 tag.</h1>
+
+<?php
+// Single-line comment
+
+/*
+ Multi-line comment
+ jdfhkf
+ djasgdjajs
+*/
+
+echo "This is PHP Syntax!";
+?>
---
+++ PHP/variabletest.php
@@ -0,0 +1,27 @@
+<?php
+// Variables, Data Types, Constants, Operators
+
+$name = "John"; // String
+$age = 25;      // Integer
+$price = 99.99; // Float
+$isAdmin = true; // Boolean
+
+echo ($name . " is " . $age . " years old.");
+
+?>
+
+<?php
+$number = 50;
+
+echo("<br/>");
+
+if($number % 2 == 0)
+{
+    echo "Even";
+}
+else
+{
+    echo "Odd";
+}
+
+?>
